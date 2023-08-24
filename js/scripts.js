b070205bb45c8692e42d1e0ec1cc6f8d5cf5ecfe
Davidvl <josuedavidvl18@gmail.com>
2023-08-24 14:00:21 -0600

    Manejando el CRUD con ayuda de jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,36 @@
 $(document).ready(function() {
     loadTasks();
 
-    $('#taskForm').submit
-})
+    $('#taskForm').submit(function(e) {
+        e.preventDefault();
+
+        let title = $('#title').val();
+        let description = $('#description').val();
+
+        $.post('save_task.php', { title, description }, function(response) {
+            if(response.status === "success") {
+                loadTasks();
+                $('#taskForm').trigger('reset');
+            } else {
+                alert(response.message);
+            }
+        }, "json");
+    });
+
+    $(document).on('click', '.delete-task', function() {
+        let element = $(this)[0];
+        let id = $(element).attr('data-id');
+
+        $.post('delete_task.php', { id }, function(response) {
+            if(response.status === "success") {
+                loadTasks();
+            } else {
+                alert(response.message);
+            }
+        }, "json");
+    });
+});
+
+function loadTasks() {
+    
+}
