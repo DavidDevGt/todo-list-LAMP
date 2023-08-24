2ac3d0bccbf9aaf2ff6984982a764b3a4c5c0509
Davidvl <josuedavidvl18@gmail.com>
2023-08-24 13:04:35 -0600

    frontend terminado

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+
+require 'connection.php'
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todo List - DavidDevGt</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <h1 class="text-center mb-4">Mi Lista de Tareas</h1>
+                <form id="taskForm">
+                    <div class="mb-3">
+                        <label for="title" class="form-label">Título</label>
+                        <input type="text" class="form-control" id="title" required placeholder="Escribe el título...">
+                    </div>
+                    <div class="mb-3">
+                        <label for="description" class="form-label">Descripción</label>
+                        <textarea class="form-control" id="description" placeholder="Escribe una descripción corta..."></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Agregar Tarea</button>
+                </form>
+                <ul id="taskList" class="list-group mt-4">
+                    <!-- Las tareas se listarán aquí -->
+                </ul>
+            </div>
+        </div>
+    </div>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.min.js"></script>
+<script src="scripts.js"></script>
+</body>
+</html>
